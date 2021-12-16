17b0ae8bc68c668d0cf449be6cf8d2bee39680c2
Tijani Ridwan <iamtridwan@gmail.com>
2021-12-16 19:00:07 +0100

    Add files via upload

---
+++ chatFlow3/app.js
@@ -0,0 +1,39 @@
+const CloseBtn = document.getElementById("closeBtn");
+const sideNav = document.getElementById("sideMenu");
+const hamburger = document.querySelector(".hamburger");
+const year = document.getElementById("year");
+// toggling  mobile nav.
+hamburger.addEventListener("click", () => {
+  sideNav.style.width = "100%";
+});
+
+CloseBtn.addEventListener("click", () => {
+  sideNav.style.width = "0px";
+});
+
+// routing
+const back = document.getElementById("back");
+back.addEventListener("click", () => {
+  window.location.href = "../chatFlow1/index.html";
+});
+
+// animation
+//animations
+let elements = document.querySelectorAll(".hidden");
+let windowHeight = window.innerHeight;
+
+function checkPosition() {
+  for (let i = 0; i < elements.length; i++) {
+    element = elements[i];
+    let topPos = element.getBoundingClientRect().top;
+    if (topPos - windowHeight <= 0) {
+      element.classList.add("animate");
+      element.classList.remove("hidden");
+    } else {
+      element.classList.remove("animate");
+      element.classList.add("hidden");
+    }
+  }
+}
+window.addEventListener("scroll", checkPosition);
+window.addEventListener("resize", checkPosition);
