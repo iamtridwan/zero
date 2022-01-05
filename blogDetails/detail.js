abf4c884726f6e096235e32c925a7ed0b9a403f3
Tijani Ridwan <iamtridwan@gmail.com>
2022-01-05 14:12:32 +0100

    comment section

--- blogDetails/detail.js
+++ blogDetails/detail.js
@@ -21,31 +21,35 @@ buttons.forEach((btn) =>
   })
 );
 
-
 // clicking love img
 
-const loveImg = document.querySelector('.like-img')
-const countOfLove = document.querySelector('.like p span')
-let loveCount = 0
-loveImg.addEventListener('click', ()=> {
-  loveCount += 1
-  countOfLove.textContent = loveCount
-})
+const loveImg = document.querySelector(".like-img");
+const countOfLove = document.querySelector(".like p span");
+let loveCount = 0;
+loveImg.addEventListener("click", () => {
+  loveCount += 1;
+  countOfLove.textContent = loveCount;
+});
 
 // clicking comment
 
 const commentImg = document.querySelector(".comment_img");
 const commentCount = document.querySelector(".comment p span");
-const form = document.querySelector('form')
-const submit = document.querySelector('input')
+const form = document.querySelector("form");
+const submit = document.querySelector("input");
+const textArea = document.querySelector("textarea");
 
-commentImg.addEventListener('click', ()=> {
-  form.style.display = "block"
-})
-let comment_count = 0
-submit.addEventListener('click', (e)=> {
-  e.preventDefault()
-  form.style.display = "none"
-  comment_count+= 1
-  commentCount.textContent = comment_count
-})
+commentImg.addEventListener("click", () => {
+  form.style.display = "block";
+});
+let comment_count = 0;
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.style.display = "none";
+  if (textArea.value.trim() !== "") {
+    comment_count += 1;
+    textArea.value = "";
+  }
+
+  commentCount.textContent = comment_count;
+});
